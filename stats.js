f4f6825468d5ad2678e9bf5155adbfb172877f61
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-22 08:24:35 -0800

    added getAberage function into stats.js

--- stats.js
+++ stats.js
@@ -1,3 +1,9 @@
 // Define a function called average which returns the average of a list of numbers.
 // Define a function called median which returns the median of an array of numbers.
 // Define a function called standardDev which returns the standard deviation of an array of numbers.
+
+const getAverage = (nums) => {
+  return nums.reduce((acc, val) => acc + val, 0) / nums.length;
+};
+
+export { getAverage };
