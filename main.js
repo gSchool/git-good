89bb919b1894c5f22394670ef61841b7948d4940
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-22 08:12:26 -0800

    made function to get the average of nums in an array

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const data = [24, 987, 23, 973, 32, 3462, 382, 53];
 
 // Find the mean, median, and standard deviation of `data` and print them to the console.
+
+const getAverage = (nums) => {
+    return nums.reduce((acc, val) => acc + val) / nums.length;
+}
+
+console.log(getAverage(data));
+
