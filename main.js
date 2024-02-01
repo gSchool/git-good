c2485c631e3a15398eb42d9c230c2c5c610c99b7
xxjrtxx <xxjrtxx@pop-os.localdomain>
2024-02-01 15:46:41 -0600

    My changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { average } from "./stats.js";
 
-const data = [24, 987, 23, 973, 32, 3462, 382, 53];
+const data = [24, 987, 23, 973, 32, 3462, 382, 53]; << WHAT DO THESE NUMBER MEAN >>
 
 // Find the mean, median, and standard deviation of `data` and print them to the console.
