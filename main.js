93dfc3ccdef0dafa3947a0db682e9f117cfcb3c6
Danny Andrews <lunde.andrews@gmail.com>
2024-02-01 16:26:42 -0500

    Break everything

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import { average } from "./stats.js";
 
 const data = [24, 987, 23, 973, 32, 3462, 382, 53];
 
-// Find the mean, median, and standard deviation of `data` and print them to the console.
+// Find the mean, median, and standard deviation of `data` and print them to the console!
