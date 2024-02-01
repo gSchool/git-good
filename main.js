81222a2298d3a1273971d0e624b64480bfc3de6f
Danny Andrews <danny-andrews@users.noreply.github.com>
2024-02-01 14:37:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { average } from "./stats.js";
+
 const data = [24, 987, 23, 973, 32, 3462, 382, 53];
 
 // Find the mean, median, and standard deviation of `data` and print them to the console.
